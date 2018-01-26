a6e6c77f2525df2032e3bec22e2559184f4ce643
Vlad Gridin <vladyslav.gridin@nuagenetworks.net>
2018-01-26 09:36:02 +0100

    Add ci gate hooks.
    Add gate_hook and post_test_hook to be able
    to configure tempest for heat.
    
    Change-Id: Iaed900f266f324fee4346f84d7796d64b3f05ded
    Depends-On: Ia3b937b0f4d23af12b273999962460150f27fe1e

---
+++ devstack/ci/post_test_hook.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# This script is executed inside post_test_hook function in devstack gate.
+
+set -ex
+
+export DEST=${DEST:-/opt/stack/new}
+sudo -E $DEST/heat/heat_integrationtests/prepare_test_env.sh
+
+set -o errexit
+if [[ "${TEMPEST_OS_TEST_TIMEOUT:-}" != "" ]] ; then
+    TEMPEST_COMMAND="sudo -H -E -u tempest OS_TEST_TIMEOUT=$TEMPEST_OS_TEST_TIMEOUT tox"
+else
+    TEMPEST_COMMAND="sudo -H -E -u tempest tox"
+fi
+cd $BASE/new/tempest
+$TEMPEST_COMMAND -evenv-tempest -- pip install ${DEST}/nuage-tempest-plugin
+echo "Checking installed Tempest plugins:"
+$TEMPEST_COMMAND -evenv-tempest -- tempest list-plugins
+
+
+r="^(?!.*"
+# Begin list of exclusions.
+
+# upstream trunk tests will not run due to template version 'pike'
+# which is not known in current release
+r="$r(?:heat_tempest_plugin\.tests\.functional\.test_create_update_neutron_trunk.*)"
+
+# End list of exclusions.
+r="$r)"
+
+r="$r^(heat_tempest_plugin\.tests\.functional\.test_create_update_neutron|nuage_tempest_plugin\.tests\.api\.(orchestration|ipv6\.vsd_managed\.test_orchestration)).*"
+echo "Running tempest with plugins and a custom regex filter"
+$TEMPEST_COMMAND -evenv-tempest -- tempest run --regex "$r" --concurrency=$TEMPEST_CONCURRENCY
+$TEMPEST_COMMAND -evenv-tempest -- tempest list-plugins
+
+
