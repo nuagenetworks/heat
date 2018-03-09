30a12540a5ee49b5f1d9060bcb6e571f67afe93b
Vlad Gridin <Vladyslav.Gridin@alcatel-lucent.com>
2018-03-09 17:06:42 +0100

    Add modification clause to ASL-2.0 license
    Change-Id: I8811d2469b3c5e4834fce541b3828ae0c7b45c58
    Closes-Bug: OPENSTACK-2123

--- nuage-heat/resources/trunk.py
+++ nuage-heat/resources/trunk.py
@@ -1,4 +1,5 @@
 # Copyright 2017 Ericsson
+# Modifications copyright 2018 Nokia
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
