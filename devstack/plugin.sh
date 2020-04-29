47e82d6550a6da444eefcc03bb867ca1d7c1f592
Lina He <lina.he@nokia.com>
2020-04-29 08:39:27 +0200

    Make heat job independent on devstack-gate
    Change-Id: I7dc27c67f84cd270338353501d66cd1829c964d5

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -29,6 +29,30 @@ dir=${GITDIR['nuage-openstack-heat']}/devstack
 if [[ "$1" == "stack" && "$2" == "install" ]]; then
     echo_summary "Installing Heat Nuage plugin"
     setup_develop ${GITDIR['nuage-openstack-heat']}
+elif [[ "$1" == "stack" && "$2" == "test-config" ]]; then
+        if is_service_enabled tempest; then
+            echo_summary "Configuring Tempest for Heat"
+            source $TOP_DIR/openrc demo demo
+
+            iniset $TEMPEST_CONFIG heat_plugin username $OS_USERNAME
+            iniset $TEMPEST_CONFIG heat_plugin password $OS_PASSWORD
+            iniset $TEMPEST_CONFIG heat_plugin project_name $OS_PROJECT_NAME
+            iniset $TEMPEST_CONFIG heat_plugin auth_url $OS_AUTH_URL
+            iniset $TEMPEST_CONFIG heat_plugin user_domain_id $OS_USER_DOMAIN_ID
+            iniset $TEMPEST_CONFIG heat_plugin project_domain_id $OS_PROJECT_DOMAIN_ID
+            iniset $TEMPEST_CONFIG heat_plugin user_domain_name $OS_USER_DOMAIN_NAME
+            iniset $TEMPEST_CONFIG heat_plugin project_domain_name $OS_PROJECT_DOMAIN_NAME
+            iniset $TEMPEST_CONFIG heat_plugin region $OS_REGION_NAME
+            iniset $TEMPEST_CONFIG heat_plugin auth_version $OS_IDENTITY_API_VERSION
+
+            source $TOP_DIR/openrc admin admin
+            iniset $TEMPEST_CONFIG heat_plugin admin_username $OS_USERNAME
+            iniset $TEMPEST_CONFIG heat_plugin admin_password $OS_PASSWORD
+
+            if is_service_enabled tls-proxy; then
+                iniset $TEMPEST_CONFIG heat_plugin ca_file $SSL_BUNDLE_FILE
+            fi
+        fi
 elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
     echo_summary "Configuring Heat Nuage plugin"
     iniset $HEAT_CONF DEFAULT plugin_dirs ${GITDIR['nuage-openstack-heat']}
